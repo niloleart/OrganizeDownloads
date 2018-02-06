94cd16058d93f249f69abb2d9ea4f54f4bb71e72
niloleart <niloleart@gmail.com>
2018-02-06 05:04:42 +0100

    TV Shows full functionalities

--- domagic.py
+++ domagic.py
@@ -1,46 +1,48 @@
 import functions as f
 
-DOWNLOADS_SRC = '/Users/niloleart/Downloads/'
-VUZE_SRC = '/Users/niloleart/Documents/Vuze Downloads/'
+DOWNLOADS_SRC = '/Users/niloleart/Desktop/test_mv_files'
+VUZE_SRC = '/Users/niloleart/Desktop/test_vuze_files'
+
+
+# show = 'Young'
+# season = '01'
+# episode = '09'
+# zip_file_path = '/Users/niloleart/Desktop/test_mv_files/Young.Sheldon.S01E05.HDTV.x264-SVA.zip'
+# sub_file_name = 'Young.Sheldon.S01E05.720p.HDTV.HEVC.x265-RMTeam.srt'
+# dst_path = '/Users/niloleart/Desktop/test_vuze_files/Young.Sheldon.S01E05.HDTV.x264-SVA[rarbg]'
+# movie_name = 'Young.Sheldon.S01E05.HDTV.x264-SVA'
 
 
 def domagic():
-    print 'Enter Serie Name and Episode (in format S__E__)'
-    serie = raw_input("Serie: ").lower()
+    show = raw_input("Show: ").lower()
+    season = raw_input("Season: ").lower()
     episode = raw_input("Episode: ").lower()
 
-    zip_file_path = f.find_zip_torent(serie, episode, '.zip', DOWNLOADS_SRC)
+    sub_file, sub_file_path = f.find_sub(DOWNLOADS_SRC)
+    if sub_file_path:
+        ### VUZE HANDLINGS
+        dst_path = f.find_folder(show, season, episode, VUZE_SRC)
+        movie_name = f.find_movie(dst_path)
+        f.move_srt(sub_file_path, dst_path, movie_name)
 
-    if zip_file_path:
+        ### TORRENT FILE HANDLING
+        torrent_file_path = f.find_torrent(show, season, episode, '.torrent', DOWNLOADS_SRC)
+        f.remove_torrent(torrent_file_path)
 
-        sub_file = f.unzip_sub(zip_file_path, DOWNLOADS_SRC)
-
-        f.remove_zip_torrent(zip_file_path)
-
-        folder_path = f.find_folder(serie, episode, VUZE_SRC)
-
-        movie_name = f.find_movie(folder_path)
-
-        f.move_srt(sub_file,folder_path,movie_name)
-
-        torrent_file_path = f.find_zip_torent(serie, episode, '.torrent', DOWNLOADS_SRC)
-
-        f.remove_zip_torrent(torrent_file_path)
 
     else:
-        sub_file = f.find_sub(DOWNLOADS_SRC)
-        if sub_file:
-            folder_path = f.find_folder(serie, episode, VUZE_SRC)
+        ### EXTRACT SRT FILE AND REMOVE ZIP
+        zip_file_path = f.find_zip(show, season, episode, 'zip', DOWNLOADS_SRC)
+        sub_file_name = f.unzip_sub(zip_file_path, DOWNLOADS_SRC)
 
-            movie_name = f.find_movie(folder_path)
+        dst_path = f.find_folder(show, season, episode, VUZE_SRC)
+        movie_name = f.find_movie(dst_path)
+        f.move_srt(sub_file_name, dst_path, movie_name)
 
-            f.move_srt(sub_file, folder_path, movie_name)
 
-            torrent_file_path = f.find_zip_torent(serie, episode, '.torrent', DOWNLOADS_SRC)
-
-            f.remove_zip_torrent(torrent_file_path)
-
-        else:
-            print 'No trobat'
+        ### REMOVE ZIP AND TORRENT
+        torrent_file_path = f.find_torrent(show, season, episode, '.torrent', DOWNLOADS_SRC)
+        f.remove_zip(zip_file_path)
+        f.remove_torrent(torrent_file_path)
 
 domagic()
--- functions.py
+++ functions.py
@@ -1,70 +1,125 @@
 import os
 import zipfile
+from zipfile import BadZipfile
 import shutil
+import sys
+from termcolor import colored, cprint
 
 
 # Returns path to Zip File
-def find_zip_torent(serie, SxxExx, extension, path):
-    for file in [x.lower() for x in os.listdir(path)]:
-        print file
-        if file.endswith(extension) and serie in file and SxxExx in file:
-            zip_path = os.path.join(path, file)
-            print 'Zip path: ' + zip_path
+def find_zip(show, season, episode, extension, path):
+    for zip_file in os.listdir(path):
+        if zip_file.lower().endswith(extension) and show.lower() in zip_file.lower() and (
+                    ('s' + season + 'e' + episode) or (season + 'x' + episode)) in zip_file.lower():
+            zip_path = os.path.join(path, zip_file)
+            done('ZIP FILE PATH:')
+            done_msg(zip_path + '\n')
             return zip_path
+    else:
+        sys.exit('Unable to find zip file' + '\n')
+
+
+def find_torrent(show, season, episode, extension, path):
+    for zip_file in os.listdir(path):
+        if zip_file.lower().endswith(extension) and show.lower() in zip_file.lower() and (
+                    ('s' + season + 'e' + episode) or (season + 'x' + episode)) in zip_file.lower():
+            zip_path = os.path.join(path, zip_file)
+            done('TORRENT FILE PATH:')
+            done_msg(zip_path + '\n')
+            return zip_path
+    else:
+        sys.exit('Unable to find TORRENT file' + '\n')
+
 
 # Extracts subtitle and returns its name
-def unzip_sub(path, root):
-    zip_ref = zipfile.ZipFile(path, 'r')
-    llista = zip_ref.namelist()
-    for sub_file in llista:
-        if sub_file.endswith('.srt') or sub_file.endswith('.SRT'):
-            try:
-                zip_ref.extract(sub_file, root)
-            finally:
-                print 'Extraccio completada: ' + sub_file
-    zip_ref.close()
-    sub_file = os.path.join(root,sub_file)
-    return sub_file
+def unzip_sub(src, dst):
+    try:
+        zip_ref = zipfile.ZipFile(src, 'r')
+        zip_files = zip_ref.namelist()
+        for arx in zip_files:
+            if arx.lower().endswith('.srt'):
+                zip_ref.extract(arx, dst)
+                done('FILE EXTRACTED:')
+                done_msg(arx + '\n')
+                zip_ref.close()
+                arx = os.path.join(dst, arx)
+                return arx
+    except zipfile.BadZipFile as e:
+        print e
+        sys.exit('Unable to extract srt' + '\n')
 
 
-def remove_zip_torrent(file):
+def remove_zip(file):
     try:
         os.remove(file)
-    finally:
-        print 'Eliminat correctament: ' + file
+        done('ZIP FILE REMOVED:')
+        done_msg(file + '\n')
+    except OSError:
+        fail('Unable to delete zip file' + '\n')
 
 
-def find_folder(serie, SxxExx, folder_path):
-    for dir in [x.lower() for x in os.listdir(folder_path)]:
-        if serie in dir.lower() and SxxExx in dir.lower():
-            if serie:
-                folder_path = os.path.join(folder_path, dir)
-                print 'Folder path: ' + folder_path
-                return folder_path
-            else:
-                print 'Folder not found'
+def remove_torrent(file):
+    try:
+        os.remove(file)
+        done('TORRENT FILE REMOVED:')
+        done_msg(file + '\n')
+    except OSError:
+        fail('Unable to delete torrent file' + '\n')
+
+
+def find_folder(show, season, episode, vuze_path):
+    for dir in os.listdir(vuze_path):
+        if show.lower() in dir.lower() and (('s' + season + 'e' + episode) or (season + 'x' + episode)) in dir.lower():
+            dst_path = os.path.join(vuze_path, dir)
+            done('VUZE FOLDER PATH:')
+            done_msg(dst_path + '\n')
+            return dst_path
+    else:
+        fail('Vuze destination folder not found' + '\n')
 
 
 def find_movie(folder_path):
-    for file in [x.lower() for x in os.listdir(folder_path)]:
-        if file.endswith('.mp4') or file.endswith('.avi') or file.endswith('.MP4') or file.endswith('.AVI') or file.endswith('.mkv') or file.endswith('.MKV'):
+    for file in os.listdir(folder_path):
+        if file.lower().endswith('.mp4') or file.endswith('.avi') or file.endswith('.mkv'):
             movie_name = os.path.splitext(file)[0]
-            print 'Movie Name: ' + movie_name
+            done('MOVIE NAME:')
+            done_msg(movie_name + '\n')
             return movie_name
+    else:
+        fail('Could not find movie file' + '\n')
 
 
 def find_sub(folder_path):
     for sub_file in os.listdir(folder_path):
-            if sub_file.endswith('.srt') or sub_file.endswith('.SRT'):
-                sub_file = os.path.join(folder_path, sub_file)
-                return sub_file
+        if sub_file.lower().endswith('.srt'):
+            sub_file_path = os.path.join(folder_path, sub_file)
+            return sub_file, sub_file_path
+    else:
+        return False, False
 
 
-def move_srt(src, dst, movie_name):
-    print src
-    print dst
-    print movie_name
+def move_srt(sub_file, dst, movie_name):
+    destination = dst + '/' + movie_name + '.srt'
     try:
-        shutil.move(src, dst + '/' + movie_name + '.srt')
-    finally:
-        print 'Mogut correctament'
+        shutil.move(sub_file, destination)
+        done('SUBTITLES FILE CORRECTLY MOVED FROM:')
+        done_msg(sub_file)
+        done('TO: ')
+        done_msg(destination + '\n')
+    except IOError:
+        sys.exit('Could not move srt file' + '\n')
+    except TypeError:
+        sys.exit('Could not move srt file' + '\n')
+
+
+def done(text):
+    cprint(text, 'blue', attrs=['reverse'])
+
+
+def done_msg(text):
+    cprint(text, 'blue', attrs=['bold'])
+
+
+def fail(text):
+    cprint(text, 'red', attrs=['reverse', 'bold'])
+    
